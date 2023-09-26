76b1a312257a7cf3028e38950fa95a1b59285b67
RATHINAKUMAR SUNDARAJAN <117304767+RathinakumarSundarajan@users.noreply.github.com>
2023-09-26 11:32:42 +0530

    Add files via upload

---
+++ src/store/index..js
@@ -0,0 +1,37 @@
+import { createSlice, configureStore } from "@reduxjs/toolkit";
+// import { createStore } from "redux";
+
+const initialState = {counter: 0, showCounter: true};
+
+const counterSlice = createSlice({
+    name: 'anyName',
+    initialState,
+    reducers:{
+        increment(state){
+            state.counter++;
+        },
+        increase(state, action){
+            state.counter = state.counter + action.payload;
+        },
+        decrement(state){
+            state.counter--;
+        },
+        reset(state){
+            state.counter = 0
+        },
+        toggleCounter(state){
+            state.showCounter = !state.showCounter;
+        },
+    }
+})
+
+
+
+const store = configureStore({
+    reducer: counterSlice.reducer
+});
+
+export const counterActions = counterSlice.actions;
+
+
+export default store;
